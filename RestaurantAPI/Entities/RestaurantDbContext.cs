b17f7245379fd7efb59740b94a34b646937d1f75
Shaqal7 <shaqal7@gmail.com>
2024-01-02 15:07:37 +0100

    Add Entities

---
+++ RestaurantAPI/Entities/RestaurantDbContext.cs
@@ -0,0 +1,28 @@
+﻿namespace RestaurantAPI.Entities
+{
+	using Microsoft.EntityFrameworkCore;
+	public class RestaurantDbContext : DbContext
+	{
+		private string _connectionString = @"Server=(localdb)\mssqllocaldb;Database=RestaurantDb;Trusted_Connection=True;";
+		public DbSet<Restaurant> Restaurants { get; set; }
+		public DbSet<Address> Addresses { get; set; }
+		public DbSet<Dish> Dishes { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Restaurant>()
+				.Property(r => r.Name)
+				.IsRequired()
+				.HasMaxLength(25);
+
+			modelBuilder.Entity<Dish>()
+				.Property(d => d.Name)
+				.IsRequired();
+		}
+
+		override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(_connectionString);
+		}
+	}
+}
