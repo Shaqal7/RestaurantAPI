ac90b8acc5f728bdc05927acdb03bf5cef7474a8
Shaqal7 <shaqal7@gmail.com>
2024-01-04 19:18:28 +0100

    fix

--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -9,14 +9,31 @@ using RestaurantAPI.Models;
 using RestaurantAPI.Models.Validators;
 using FluentValidation.AspNetCore;
 using RestaurantAPI;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var authenticationSettings = builder.Configuration.GetSection("Authentication").Get<AuthenticationSettings>();
+var authenticationSettings = new AuthenticationSettings();
 
+builder.Configuration.GetSection("Authentication").Bind(authenticationSettings);
+
+builder.Services.AddAuthentication(options =>
+{
+	options.DefaultAuthenticateScheme = "Bearer";
+	options.DefaultChallengeScheme = "Bearer";
+	options.DefaultScheme = "Bearer";
+}).AddJwtBearer(cfg =>
+{
+	cfg.RequireHttpsMetadata = false;
+	cfg.SaveToken = true;
+	cfg.TokenValidationParameters = new TokenValidationParameters()
+	{
+		ValidIssuer = authenticationSettings.JwtIssuer,
+		ValidAudience = authenticationSettings.JwtIssuer,
+		IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationSettings.JwtKey))
+	};
+});
 // Add services to the container.
 builder.Logging.ClearProviders();
 builder.Logging.SetMinimumLevel(LogLevel.Trace);
@@ -37,21 +54,7 @@ builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
 builder.Services.AddScoped<IValidator<RegisterUserDto>, RegisterUserDtoValidator>();
 builder.Services.AddScoped<ErrorHandlingMiddleware>();
 builder.Services.AddScoped<RequestTimeMiddleware>();
-builder.Configuration.Bind(nameof(authenticationSettings), authenticationSettings);
-builder.Services.AddAuthentication(options =>
-{
-	options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-	options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-	options.TokenValidationParameters = new TokenValidationParameters()
-	{
-		ValidateIssuer = true,
-		ValidateAudience = false,
-		ValidateLifetime = true,
-		IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationSettings.JwtKey))
-	};
-});
+builder.Services.AddSingleton(authenticationSettings);
 
 var app = builder.Build();
 
