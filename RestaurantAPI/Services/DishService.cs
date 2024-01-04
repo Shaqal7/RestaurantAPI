adada3cfc03a2f787b8187cf5a4346830f4f854b
Shaqal7 <shaqal7@gmail.com>
2024-01-04 14:56:22 +0100

    Update DishService.cs

--- RestaurantAPI/Services/DishService.cs
+++ RestaurantAPI/Services/DishService.cs
@@ -16,10 +16,7 @@ namespace RestaurantAPI.Services
 		}
 		public int Create(int restaurantId, CreateDishDto dto)
 		{
-			var restaurant = _context.Restaurants.FirstOrDefault(r => r.Id == restaurantId);
-			if (restaurant is null)
-				throw new NotFoundException("Restaurant not found");
-
+			var restaurant = _context.Restaurants.FirstOrDefault(r => r.Id == restaurantId) ?? throw new NotFoundException("Restaurant not found");
 			var dishEntity = _mapper.Map<Dish>(dto);
 			dishEntity.RestaurantId = restaurantId;
 
