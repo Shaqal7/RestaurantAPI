c2f75b9045d3fb89a791091806ea20118dfe2925
Shaqal7 <shaqal7@gmail.com>
2024-01-08 20:41:43 +0100

    Walidacja query

---
+++ RestaurantAPI/Models/Validators/RestaurantQueryValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace RestaurantAPI.Models.Validators
+{
+	public class RestaurantQueryValidator : AbstractValidator<RestaurantQuery>
+	{
+		private int[] allowedPageSizes = new[] { 5, 10, 15 };
+		public RestaurantQueryValidator()
+		{
+			RuleFor(x => x.PageNumber).GreaterThanOrEqualTo(1);
+			RuleFor(x => x.PageSize).Custom((value, context) =>
+			{
+				if (!allowedPageSizes.Contains(value))
+				{
+					context.AddFailure("PageSize", $"PageSize must in [{string.Join(",", allowedPageSizes)}]");
+				}
+			});
+		}
+	}
+}
