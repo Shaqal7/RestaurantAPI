5e6a7af8b22ab7d5694ece20f38dc3f1cd964b7e
Shaqal7 <shaqal7@gmail.com>
2024-01-09 18:43:10 +0100

    HomeController

---
+++ RestaurantAPI/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantAPI.Controllers
+{
+	public class HomeController : ControllerBase
+	{
+		[HttpGet("/")]
+		public ActionResult Get()
+		{
+			return Ok("Hello from Restaurant API");
+		}
+	}
+}
